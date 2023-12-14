9738f728a4d3cbb9d390e2727d9f50d3e91a47be
Christophe Domergue <christophe.domergue@zenika.com>
2023-12-14 23:23:01 +0100

    added experiences

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -1,22 +1,182 @@
-import {ChangeDetectionStrategy, Component, effect, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, effect, inject, Signal} from '@angular/core';
 import {LanguageService} from "../language.service";
+import {NgOptimizedImage} from "@angular/common";
+import {MarkdownComponent, MarkdownModule} from "ngx-markdown";
+import exp from "node:constants";
 
 @Component({
   selector: 'app-experiences',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    MarkdownComponent
+  ],
   templateUrl: './experiences.component.html',
+  styleUrls: ['experiences.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExperiencesComponent {
   currentLanguage = inject(LanguageService).currentLanguage;
 
+  experiencesFr: Experience[] = [
+    {
+      id: 1,
+      duration: '6 ans',
+      start: 'Décembre 2017',
+      end: 'Décembre 2023',
+      title: 'Développeur Full stack Angular',
+      logoUrl: 'assets/logo-arval.png',
+      companyName: 'Arval',
+      body: [{
+        title: 'Contexte :',
+        text: `Arval Connect est une plateforme de télématique embarquée qui permet aux entreprises louant des
+        véhicules chez Arval de suivre précisément leur utilisation.`
+      },
+        {
+          title: 'Tâches réalisées :',
+          text: '- \\- Amélioration et ajouts de nouvelles fonctionnalités aux frontaux (2 fronts différents)\n' +
+            '- \\- Refonte UI / UX de l’application\n' +
+            '- \\- Migration des frontaux depuis AngularJS vers Angular\n' +
+            '- \\- Réalisation des tests unitaires et d\'intégration pour assurer la qualité du\ncode.\n' +
+            '- \\- Co-Lead Tech Front à partir de 2023\n' +
+            '- \\- Création et modification des différents endpoints REST en Java.'
+        },
+        {
+          title: 'Environnement technique',
+          text: 'Agilité, Angular 16, AngularJS, NX, Angular Testing Framework, Java 17,' +
+            ' Spring Boot, Maven, Jenkins,' +
+            ' Lambda, OracleSQL, Opensearch, Redis, AWS (nombreux services),' +
+            ' Docker, Atlassian(Bitbucket, Confluence, JIRA)'
+        }]
+    },
+    {
+      id: 2,
+      duration: '4 mois',
+      start: 'Aout 2014',
+      end: 'Novembre 2017',
+      title: 'Développeur Angular',
+      logoUrl: 'assets/logo-bnp.png',
+      companyName: 'BNP Paribas',
+      body: [{
+        title: 'Tâches réalisées :',
+        text: '- \\- Réalisation d\'une application web permettant la gestion de syndication\n' +
+          'bancaire (Angular 4)\n'
+      },
+        {
+          title: 'Environnement technique',
+          text: 'Angular (4+), Angular Material, Node (yarn, npm),' +
+            ' Webpack, Cucumber, protractor, Java 8, Spring Boot, Maven, Jenkins,' +
+            ' Docker, Rancher, Git'
+        }]
+    },
+    {
+      id: 3,
+      duration: '3 ans',
+      start: 'Septembre 2014',
+      end: 'Septembre 2017',
+      title: 'Développeur en Alternance',
+      logoUrl: 'assets/logo-zenika.png',
+      companyName: 'Zenika',
+      body: [{
+        title: 'Tâches réalisées :',
+        text: '- \\- 2017: Réalisation d\'une application web de gestion interne (Angular)\n' +
+          '- \\- 2015: Réalisation d\'une application web de gestion interne (React)\n' +
+          '- \\- 2014: Amélioration d\'une application web interne de réseau social\n' +
+          '(AngularJS)\n'
+      }]
+    }
+  ];
+
+  experiencesEn: Experience[] = [
+    {
+      id: 1,
+      title: 'Full Stack Angular Developer',
+      duration: '6 years',
+      start: 'December 2017',
+      end: 'December 2023',
+      logoUrl: 'assets/logo-arval.png',
+      companyName: 'Arval',
+      body: [{
+        title: 'Context:',
+        text: `Arval Connect is an embedded telematics platform that allows companies leasing vehicles
+        from Arval to precisely track their usage.`
+      },
+        {
+          title: 'Tasks performed:',
+          text: '- \\- Improvement and addition of new features to the frontends (2 different fronts)\n' +
+            '- \\- UI / UX redesign of the application\n' +
+            '- \\- Migration of frontends from AngularJS to Angular\n' +
+            '- \\- Performing unit and integration tests to ensure code quality.\n' +
+            '- \\- Co-Lead Tech Front starting from 2023\n' +
+            '- \\- Creation and modification of different REST endpoints in Java.'
+        },
+        {
+          title: 'Technical environment',
+          text: 'Agility, Angular 16, AngularJS, NX, Angular Testing Framework, Java 17,' +
+            ' Spring Boot, Maven, Jenkins,' +
+            ' Lambda, OracleSQL, Opensearch, Redis, AWS (numerous services),' +
+            ' Docker, Atlassian(Bitbucket, Confluence, JIRA)'
+        }]
+    },
+{
+  id: 2,
+  duration: '4 months',
+  start: 'August 2014',
+  end: 'November 2017',
+  title: 'Angular Developer',
+  logoUrl: 'assets/logo-bnp.png',
+  companyName: 'BNP Paribas',
+  body: [{
+    title: 'Tasks performed:',
+    text: '- \\- Development of a web application for managing bank syndication\n' +
+      '(Angular 4)\n'
+  },
+    {
+      title: 'Technical environment',
+      text: 'Angular (4+), Angular Material, Node (yarn, npm),' +
+        ' Webpack, Cucumber, protractor, Java 8, Spring Boot, Maven, Jenkins,' +
+        ' Docker, Rancher, Git'
+    }]
+},
+{
+  id: 3,
+  duration: '3 years',
+  start: 'September 2014',
+  end: 'September 2017',
+  title: 'Developer in Apprenticeship',
+  logoUrl: 'assets/logo-zenika.png',
+  companyName: 'Zenika',
+  body: [{
+    title: 'Tasks performed:',
+    text: '- \\- 2017: Development of an internal management web application (Angular)\n' +
+      '- \\- 2015: Development of an internal management web application (React)\n' +
+      '- \\- 2014: Improvement of an internal social network web application\n' +
+      '(AngularJS)\n'
+  }]
+}
+  ];
+  experiences: Signal<Experience[]>;
+
   constructor() {
     effect(() => {
       this.setLanguage(this.currentLanguage());
     }, { allowSignalWrites: true });
+    this.experiences = computed(() => this.currentLanguage() === 'french' ? this.experiencesFr : this.experiencesEn)
   }
 
   private setLanguage(current: "french" | "english") {
   }
+
+  protected readonly exp = exp;
+}
+
+type Experience = {
+  id: number;
+  title: string;
+  body: ({ text: string; title: string })[];
+  logoUrl: string;
+  duration: string,
+  start: string,
+  end: string,
+  companyName: string
 }
