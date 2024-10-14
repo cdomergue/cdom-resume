0bd855870354880c1bb33055b5ed7bcc64aac8f5
Christophe Domergue <domergue.christophe@gmail.com>
2024-10-14 18:52:46 +0200

    update intro

--- src/app/app.data.ts
+++ src/app/app.data.ts
@@ -2,7 +2,13 @@ import { ProductType } from './shared/store/product/models/product-type';
 
 export const AppTranslationFr = {
   name: 'Christophe Domergue',
-  intro: 'Développeur d’origine Full-Stack avec une grande appétence pour le développement web, spécialisé en Angular.',
+  intro: `Je suis un développeur front-end spécialisé en Angular avec une expérience full-stack.
+
+J'ai travaillé sur des projets complexes dans les secteurs bancaire et de la télématique embarquée, menant des refontes UI/UX et des migrations technologiques importantes.
+J'ai également pris des responsabilités de co-lead technique, garantissant la qualité et la performance des projets.
+
+Passionné par le développement web, je m'efforce d'offrir des solutions modernes et efficaces pour répondre aux besoins de mes clients.
+`,
   title: 'Développeur Front-End',
   experiences: 'Mes expériences',
   education: 'Études',
@@ -34,7 +40,13 @@ export const AppTranslationFr = {
 
 export const AppTranslationEn = {
   name: 'Christophe Domergue',
-  intro: 'Full-Stack developer with a strong interest in web development, specializing in Angular.',
+  intro: `I am a front-end developer specialized in Angular with full-stack experience.
+
+I have worked on complex projects in the banking and embedded telematics sectors, leading UI/UX redesigns and significant technological migrations.
+I have also taken on co-lead technical responsibilities, ensuring the quality and performance of the projects.
+
+Passionate about web development, I strive to provide modern and efficient solutions to meet my clients' needs.
+`,
   title: 'Front-End Developer',
   experiences: 'Work Experiences',
   education: 'Education',
